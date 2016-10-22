45e1d29724c32faf2a9d8bbd22952f0223ce64c2
Aaron <ledzeppelindrummer@gmail.com>
2016-10-22 12:26:11 -0400

    Completed ticker animation, added reddit support

--- ticker.py
+++ ticker.py
@@ -1,5 +1,8 @@
+# -*- coding: utf-8 -*-
+
 from Tkinter import Frame, Tk, Label
 import tkFont
+import praw
 
 
 class Ticker(Frame):
@@ -9,15 +12,18 @@ class Ticker(Frame):
         self._offsety = 0
         self.parent = parent #sets parent frame, the default TK window.
         self.parent.title("TICKER WINDOW")
-        font = tkFont.Font(family="Hasteristico light", size=80, weight="bold")
+        font = tkFont.Font(family="Arial Rounded MT Bold", size=30)
         self.label = Label(self, text="", font=font, fg="#606060", bg="#646464")
         self.label.pack()
         self.configure(background="#646464")
         self.parent.overrideredirect(True)
         self.label.bind("<Button-1>", self.on_click)
+        self.parent.bind("<Button-1>", self.on_click)
+        self.parent.bind("<B1-Motion>", self.on_drag)
         self.label.bind("<B1-Motion>", self.on_drag)
-        self.parent.wm_attributes("-transparentcolor", "#646464")
-        self.parent.attributes('-alpha', 0.9) #sets background alpha channel
+        self.label.bind("<Button-3>", self.on_exit)
+        self.parent.wm_attributes("-transparentcolor", "#646464") #sets specific color as transparent.
+        #self.parent.attributes('-alpha', 0.) #sets background alpha channel
         self.pack(expand=True) #pack frame
 
     def on_click(self, e):
@@ -35,36 +41,54 @@ class Ticker(Frame):
         self.label.pack()
         self.pack(expand=True)
 
+    def on_exit(self, e):
+        #todo exit application
+        self.parent.quit()
 
-class NewsObject:
+class TextObject: #for right now this will not support URL clicking. This is just a widget for displaying the text
     index=0
+    start_index=0
     str_builder=""
-    def __init__(self, string, url=""):
+    def __init__(self, string, max_letters=65):
         self.string = string
-        self.url=url
+        self.max_letters = max_letters
+        self.index = max_letters
+        self.str_builder = self.string[0:self.max_letters]
 
-    def get_next_fragment(self): #todo, remove a letter from the beginning after MAX_LETTERS has been added
+    def get_next_fragment(self): #
         if self.index < len(self.string):
-            self.str_builder += self.string[self.index]
-            self.index+=1
+            #print "%d" % self.index
+            #adds chars one at a time to the str_builder string.
+            # Every time it does this it also removes one from the front of the string
+            self.str_builder =  self.str_builder[1:] + self.string[self.index]
+            self.index += 1 #increase the string index to grab the next char
             return self.str_builder
         else:
             self.index=0
-            self.str_builder = " "
+            #self.start_index=0
+            #self.str_builder = " "
             return self.str_builder
 
+def connect_to_reddit():
+    r = praw.Reddit("Reddit News Ticker")
+    posts = r.get_subreddit("news").get_hot(limit=3)
+    title_string = ""
+    for post in posts:
+        title_string += post.title.upper() + u"\u25BA"
+    return TextObject(title_string)
 
 
+#called once every n milliseconds by the graphics thread, wakes this thread to start changing the text
 def update_event():
     ticker_window.update_label(news_tick.get_next_fragment())
-    ticker_window.after(600, update_event)
+    ticker_window.after(400, update_event)
 
 #start window
-news_tick = NewsObject("Trump Ruined Ur Mums Pussy")
+news_tick = connect_to_reddit()
 root_frame = Tk()
 root_frame.resizable(0,0)
 ticker_window = Ticker(root_frame) #create instance of ticker frame window and pass in Tk root window
-root_frame.after(600, update_event)
+root_frame.after(400, update_event)
 root_frame.mainloop()
 
 
